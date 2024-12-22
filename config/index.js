63ce2237367878fd69c46fc408d3d61dbe74cca8
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-22 20:48:42 +0530

    setup google auth details

--- config/index.js
+++ config/index.js
@@ -12,6 +12,11 @@ const config = {
     },
     auth : {
         jwtSecret: process.env.JWT_SECRET ,
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackUrl: process.env.GOOGLE_CALLBACK_URL,
+        },
     } ,
 };
 
