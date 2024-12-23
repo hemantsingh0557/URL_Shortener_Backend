6f7e5a46e7449881ee74dae5047928e5776823d5
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-23 14:44:13 +0530

    also add jsonwebtoken

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,23 +1,29 @@
-
 import express from "express";
 import passport from "passport";
-
+import { generateJWTAccessToken } from "../utils/helperFunctions.js";
+import { createErrorResponse, createSuccessResponse } from "../utils/responseHelper.js";
+import { MESSAGES } from "../utils/messages.js";
+import { CONSTANTS } from "../utils/constants.js";
 
 export const authRoutes = express.Router();
 
-
 authRoutes.get("/google", passport.authenticate("google", {
-    scope: ["profile", "email"], 
+    scope: ["profile", "email"],
 }));
 
-authRoutes.get("/google/callback",
-    passport.authenticate("google", { failureRedirect: "/" }),
-    (req, res) => {
-        if (!req.user) {
-            return res.status(400).json({ message: "Authentication failed" });
-        }
-        res.json({ message: "Logged in successfully", user: req.user });
-    },
-);
-
-
+authRoutes.get("/google/callback", passport.authenticate("google", { failureRedirect: "/" }), async(req, res) => {
+    if (!req.user) {
+        return res.status(401).json(createErrorResponse(MESSAGES.AuthenticationFailed, CONSTANTS.ERROR_TYPES.UNAUTHORIZED));
+    }
+    const { googleId, email, name } = req.user;
+    const tokenPayload = {
+        id: googleId,
+        email,
+        name,
+    };
+    const token = generateJWTAccessToken(tokenPayload);
+    return res.json(createSuccessResponse(MESSAGES.LoggedInSuccessfully, {
+        token,
+        user: req.user,
+    }));
+});
