2ce3daa28df13c419ea8864d011f6b50f1bf75b6
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-29 23:27:23 +0530

    add messages

--- utils/messages.js
+++ utils/messages.js
@@ -2,5 +2,9 @@
 export const MESSAGES = {
     AuthenticationFailed: "Authentication failed",
     LoggedInSuccessfully: "Logged in successfully",
+
+    LONG_URL_ALREADY_SHORTENED : "This long URL has already been shortened for your account.",
+    ALIAS_ALREADY_EXISTS: "This custom alias is already taken. Please choose a different one.",
+    SHORT_URL_CREATED: "Your short URL has been successfully created.",
 };
 
