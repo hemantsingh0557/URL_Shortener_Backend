a159525bfd2dcd27710dba30597b895cb37a544f
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-30 23:18:14 +0530

    make a fucntion to update details

--- services/shortUrlServices.js
+++ services/shortUrlServices.js
@@ -6,3 +6,5 @@ export const shortUrlServices = {} ;
 shortUrlServices.findOne = async(criteria) => ShortURLModel.findOne(criteria) ; 
 
 shortUrlServices.create = async(payload) => ShortURLModel.create(payload) ;
+
+shortUrlServices.update = async(criteria , payload) => ShortURLModel.findOneAndUpdate(criteria, payload, { new: true });   
