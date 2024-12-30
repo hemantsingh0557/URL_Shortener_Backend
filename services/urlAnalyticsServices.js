92fd188ca16d67bd4bf0785ef9176ff0d9c4b660
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-30 23:18:30 +0530

    make urlAnalytics servcie

---
+++ services/urlAnalyticsServices.js
@@ -0,0 +1,9 @@
+import { UrlAnalyticsModel } from "../models/index.js";
+
+
+export const urlAnalyticsServices = {} ;
+
+
+urlAnalyticsServices.findOne = async(criteria) => UrlAnalyticsModel.findOne(criteria) ;
+
+urlAnalyticsServices.create = async(criteria) => UrlAnalyticsModel.create(criteria) ;
