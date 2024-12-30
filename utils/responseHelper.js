dbed321086dcaa1a9a6f89f4967b31c83d006211
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-30 13:23:28 +0530

    change in response helper

--- utils/responseHelper.js
+++ utils/responseHelper.js
@@ -1,64 +1,75 @@
-import { CONSTANTS } from "./constants.js";
+
 
 const RESPONSE = {
     ERROR: {
-        DATA_NOT_FOUND: (msg = "") => ({
-            statusCode: 404,
-            message: msg,
-            status: false,
-            type: CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND,
-        }),
-        BAD_REQUEST: (msg = "", data) => {
+        DATA_NOT_FOUND: function(msg) {
+            return {
+                statusCode: 404,
+                message: msg || "",
+                status: false,
+                type: "DATA_NOT_FOUND",
+            };
+        },
+        BAD_REQUEST: function(msg, data) {
             let obj = {
                 statusCode: 400,
                 status: false,
-                message: msg,
-                type: CONSTANTS.ERROR_TYPES.BAD_REQUEST,
+                message: msg || "",
+                type: "BAD_REQUEST",
             };
             if (data) {
                 obj = { ...obj, data };
             }
             return obj;
         },
-        ALREADY_EXISTS: (msg = "") => ({
-            statusCode: 400,
-            message: msg,
-            status: false,
-            type: CONSTANTS.ERROR_TYPES.ALREADY_EXISTS,
-        }),
-        FORBIDDEN: (msg = "") => ({
-            statusCode: 403,
-            message: msg,
-            status: false,
-            type: CONSTANTS.ERROR_TYPES.FORBIDDEN,
-        }),
-        INTERNAL_SERVER_ERROR: (msg = "") => ({
-            statusCode: 500,
-            message: msg,
-            status: false,
-            type: CONSTANTS.ERROR_TYPES.INTERNAL_SERVER_ERROR,
-        }),
-        UNAUTHORIZED: (msg = "") => ({
-            statusCode: 401,
-            message: msg,
-            status: false,
-            type: CONSTANTS.ERROR_TYPES.UNAUTHORIZED,
-        }),
+        ALREADY_EXISTS: function(msg) {
+            return {
+                statusCode: 400,
+                message: msg || "",
+                status: false,
+                type: "ALREADY_EXISTS",
+            };
+        },
+        FORBIDDEN: function(msg) {
+            return {
+                statusCode: 403,
+                message: msg || "",
+                status: false,
+                type: "FORBIDDEN",
+            };
+        },
+        INTERNAL_SERVER_ERROR: function(msg) {
+            return {
+                statusCode: 500,
+                message: msg || "",
+                status: false,
+                type: "INTERNAL_SERVER_ERROR",
+            };
+        },
+        UNAUTHORIZED: function(msg) {
+            return {
+                statusCode: 401,
+                message: msg || "",
+                status: false,
+                type: "UNAUTHORIZED",
+            };
+        },
     },
+
     SUCCESS: {
-        MISSCELANEOUSAPI: (msg = "", data) => {
+        GENERIC_SUCCESS: function(msg, data) {
             let obj = {
                 statusCode: 200,
                 status: true,
-                message: msg,
-                type: CONSTANTS.SUCCESS.SUCCESS,
+                message: msg || "",
+                type: "SUCCESS",
             };
             if (data) {
                 obj = { ...obj, data };
             }
             return obj;
         },
-        WITHOUTSTATUSRES: (data) => {
+        WITHOUT_STATUS_RES: function(data) {
             let obj = {};
             if (data) {
                 obj = { ...data };
@@ -67,30 +78,25 @@ const RESPONSE = {
         },
     },
 };
-  
-/**
- * function to create a valid SUCCESS response object.
- * message that has to be pass in the response object.
- */
-function createSuccessResponse(message, data) {
-    return RESPONSE.SUCCESS.MISSCELANEOUSAPI(message, data);
+
+
+export function createSuccessResponse(message, data) {
+    return RESPONSE.SUCCESS.GENERIC_SUCCESS(message, data);
 }
 
-/**
- * function to create a valid SUCCESS response object without status data.
- * data that has to be sent in the response object.
- */
-function createSuccessResponseWithoutStatus(data) {
-    return RESPONSE.SUCCESS.WITHOUTSTATUSRES(data);
+export function createSuccessResponseWithoutStatus(data) {
+    return RESPONSE.SUCCESS.WITHOUT_STATUS_RES(data);
 }
 
-/**
- * function to create a valid ERROR response object.
- * message that has to be pass in the response object.
- */
-function createErrorResponse(message, errorType, data) {
-    return RESPONSE.ERROR[errorType](message, data);
+
+export function createErrorResponse(message, errorType, data) {
+    if (RESPONSE.ERROR[errorType]) {
+        return RESPONSE.ERROR[errorType](message, data);
+    } else {
+        return RESPONSE.ERROR.INTERNAL_SERVER_ERROR("An unknown error occurred.");
+    }
 }
-  
-export { createErrorResponse, createSuccessResponse, createSuccessResponseWithoutStatus };
-  
+
+
+
+
