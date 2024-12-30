0e3df021c827f710c8000b021a93b30b90a69ff3
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-30 14:03:38 +0530

    add mesages relaetd to get long url

--- utils/messages.js
+++ utils/messages.js
@@ -6,5 +6,7 @@ export const MESSAGES = {
     LONG_URL_ALREADY_SHORTENED : "This long URL has already been shortened for your account.",
     ALIAS_ALREADY_EXISTS: "This custom alias is already taken. Please choose a different one.",
     SHORT_URL_CREATED: "Your short URL has been successfully created.",
+    LONG_URL_FOUND: "The long URL was successfully found.",
+    LONG_URL_NOT_FOUND: "The provided short URL could not be found the long url.",
 };
 
