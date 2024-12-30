42965b86e9efbb2a81df1d779cbf357765fe7d76
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-12-30 17:55:28 +0530

    add os device and ip and redirect the url

--- startup/expressStartup.js
+++ startup/expressStartup.js
@@ -6,18 +6,30 @@ import { authRoutes } from "../routes/authRoute.js";
 import passport from "passport";
 import session from "express-session";
 import config from "../config/index.js";
-
+import { UAParser } from "ua-parser-js";
 
 const handler = (controller) =>{
     return (req , res) => {
+        const userAgentString = req.headers["user-agent"];
+        const userIpAddress = req.headers["x-forwarded-for"] || req.ip;
+        const uaParser = new UAParser();
+        const userAgentData = uaParser.setUA(userAgentString).getResult();
+        const userDeviceType = userAgentData.device.type || "desktop"; // Mobile, tablet, or desktop
+        const userOsType = userAgentData.os.name || "Unknown"; //  Windows, Android, macOS, iOS
         const payload = {
-            ...(req.body || {}) ,
-            ...(req.params || {}) ,
-            ...(req.query || {}) ,
-            userId : req.userId ,
-        } ;
+            ...(req.body || {}),
+            ...(req.params || {}),
+            ...(req.query || {}),
+            userId: req.userId, 
+            userIpAddress, 
+            userDeviceType, 
+            userOsType, 
+        };
         controller(payload)
             .then(async(result) => {
+                if (result.data && result.data.redirectUrl) {
+                    return res.redirect(result.data.redirectUrl) ; 
+                }
                 res.status(result.statusCode).json(result) ;
             })
             .catch(async(error) => {
