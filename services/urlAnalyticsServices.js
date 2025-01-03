561a1ebfdb63e05125597782c69598562dd24154
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-01-03 23:32:32 +0530

    add new function

--- services/urlAnalyticsServices.js
+++ services/urlAnalyticsServices.js
@@ -7,3 +7,5 @@ export const urlAnalyticsServices = {} ;
 urlAnalyticsServices.findOne = async(criteria) => UrlAnalyticsModel.findOne(criteria) ;
 
 urlAnalyticsServices.create = async(criteria) => UrlAnalyticsModel.create(criteria) ;
+
+urlAnalyticsServices.aggregate = async(criteria) => UrlAnalyticsModel.aggregate(criteria) ;
